69f725eae77cf979e37e8263931a7f01a4747aaf
Codeyellow1338 <Codeyellow93@gmail.com>
2023-04-15 19:07:03 +0700

    NSB

--- main.py
+++ main.py
@@ -1,32 +1,8 @@
 from flask import Flask, render_template
-import requests
-import random
-from datetime import datetime
 
 app = Flask(__name__)
 
-
-
-@app.route('/')
-def index():
-   return render_template('index.html', 
-                          title = 'Новостной сайт!', 
-                          text = 'Скоро тут будут новости!')
-@app.route('/news')
-def news():
-   return """<!DOCTYPE html>
-   <html>
-     <head>
-        <meta charset="utf-8">
-       <title>Новости</title>
-     </head>
-     <body>
-       <h1>Новости</h1>
-     </body>
-   </html>"""
-@app.route('/news_detail/<int:id>')
-def news_detail(id):
-   news = [{'title': 'Удивительное событие в школе',
+news = [{'title': 'Удивительное событие в школе',
          'text': 'Вчера в местной школе произошло удивительное событие - все '
                  'ученики одновременно зевнули на уроке математики. '
                  'Преподаватель был так поражен этим коллективным зевком, '
@@ -46,32 +22,21 @@ def news_detail(id):
                  'в том числе - пакетики конфет и игрушки в виде косточек. '
                  'Конкурс вызвал большой интерес у посетителей парка, '
                  'и его решили повторить в более масштабном формате.'}]
-   context = news[id]
-   return render_template('news_detail.html', **context)
-@app.route('/category/<string:name>')
-def category_detail(name):
-   return f"""
-   <!DOCTYPE html>
-   <html>
-     <head>
-       <title>Категория {name}</title>
-     </head>
-     <body>
-     <h1>Категория {name} <h1>
-     </body>
-   </html>
-   """
 
-   primes = []
-   number = 2
-   while len(primes) < n:
-       for i in range(2, number):
-           if number % i == 0:
-               break
-       else:
-           primes.append(number)
-       number += 1
-   return ' '.join([str(i) for i in primes])
+
+@app.route('/')
+def index():
+    return render_template('index.html',
+                           news=news)
+
+
+@app.route('/news_detail/<int:id>')
+def news_detail(id):
+    title = news[id]['title']
+    text = news[id]['text']
+    return render_template('news_detail.html',
+                           title=title,
+                           text=text)
 
 
 if __name__ == '__main__':
