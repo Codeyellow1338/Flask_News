7f36555d70cd5e5a43f4320992d3cf7d8f47669e
Codeyellow1338 <Codeyellow93@gmail.com>
2023-04-20 19:21:06 +0700

    main and other

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, url_for
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField, TextAreaField
+from wtforms.validators import Length, DataRequired
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'SECRET_KEY'
 
 news = [{'title': 'Удивительное событие в школе',
          'text': 'Вчера в местной школе произошло удивительное событие - все '
@@ -24,6 +28,18 @@ news = [{'title': 'Удивительное событие в школе',
                  'и его решили повторить в более масштабном формате.'}]
 
 
+class NewsForm(FlaskForm):
+    title = StringField(
+        'Название',
+        validators=[DataRequired(message="Поле не должно быть пустым"),
+                    Length(max=255, message='Введите заголовок длиной до 255 символов')]
+    )
+    text = TextAreaField(
+        'Текст',
+        validators=[DataRequired(message="Поле не должно быть пустым")])
+    submit = SubmitField('Добавить')
+
+
 @app.route('/')
 def index():
     return render_template('index.html',
@@ -37,6 +53,18 @@ def news_detail(id):
     return render_template('news_detail.html',
                            title=title,
                            text=text)
+
+
+@app.route('/add_news', methods=['GET', 'POST'])
+def add_news():
+    form = NewsForm()
+    if form.validate_on_submit():
+        title = form.title.data
+        text = form.text.data
+        news.append({'title': title, 'text': text})
+        return redirect(url_for('index'))
+    return render_template('add_news.html',
+                           form=form)
 
 
 if __name__ == '__main__':
